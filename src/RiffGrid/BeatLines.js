b2ceff3cd23247b20902b230a6fdea71c7ae6f29
kirkjerk <kirkjerk@gmail.com>
2020-08-29 17:33:20 -0400

    fixing case where beat length of 0 crashed...

--- src/RiffGrid/BeatLines.js
+++ src/RiffGrid/BeatLines.js
@@ -6,6 +6,7 @@ import { beats2Pixels } from '../Utils.js';
 
 const BeatLines = ({ totalBeats, measureLengthInBeasts, BPM }) => {
     // fill array from 1 to beats - 1 so we can do a nice little map...
+    if (totalBeats <= 0) return null;
     const counts = Array.from(Array(totalBeats - 1), (_, i) => i + 1);
     return counts.map((b) => {
         const style = `${styles.beatLine} ${b % measureLengthInBeasts === 0 ? styles.measureLine : ''}`;
